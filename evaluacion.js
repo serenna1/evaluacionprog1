28bc173e931ceb611a7372a128d7dd1c5ff0b509
serenna1 <rotondaserena3@gmail.com>
2025-06-26 22:01:31 -0300

    entrga

--- evaluacion.js
+++ evaluacion.js
@@ -1,3 +1,35 @@
 /*
 * evaluacion de programacion 26/6
+competencia de saltos 
+hay que similuar una competencia de salto an largo entre varios atletas.
+8 atlletas hacen 3 intentos de saltos(medidos en metros aleatorios entre 5 y 10 )
+guardar e mejor salto de cada uno 
+gana el de salto mas largo 
+mostrar el ganador y la ista total de resultados 
+funciones a crear:crearAtletas, reaizarSaltos, registrarMejor, mostrarGanador
 */
+function competenciasDeSaltos(){
+ let atletas=[1, 2, 3, 4, 5, 6, 7, 8];
+ let mejorSalto = 0;
+ let mejorAtleta = 0;
+ 
+ 
+ 
+ for(let i = 0; i <atletas.length; i++){
+    for(let e = 0; e < 3; e++ ){
+        let metroDelSalto = Math.floor(Math.random() * 10 + 5);
+        console.log(`El atleta ${atletas[i]} Recorrio tantos metros: ${metroDelSalto}`);
+        
+       if(metroDelSalto > mejorSalto){
+        mejorSalto = metroDelSalto;
+       }
+    
+    }
+     console.log(`El mejor salto del atleta ${atletas[i]} es ${mejorSalto} `);
+     mejorSalto = 0;
+
+       
+ }
+ 
+}
+competenciasDeSaltos();
